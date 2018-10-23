ea48e44b541f98f07178625a8bd7bcb15d514c95
Sylvain Albasser <sylvain.albasser@syentys.com>
2018-10-23 17:21:48 +0200

    migration: Add complete_default_code

--- Addons/custom/product_category_default_code/models/product.py
+++ Addons/custom/product_category_default_code/models/product.py
@@ -7,3 +7,15 @@ class ProductCategory(models.Model):
     _inherit = "product.category"
     
     default_code = fields.Char(u'Référence interne')
+	complete_default_code = fields.Char(
+		u'Référence interne complète', compute='_compute_complete_default_code',
+		store=True)
+
+    @api.depends('default_code', 'parent_id.complete_default_code')
+    def _compute_complete_default_code(self):
+        for category in self:
+            if category.parent_id:
+                category.complete_default_code = '%s%s' % (category.parent_id.complete_default_code or '', category.default_code or '')
+            else:
+				category.complete_default_code = category.default_code or ''
+
