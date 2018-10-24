4512d436f73b9d4da30e597ae38eb965ccfd2a25
Sylvain Albasser <sylvain.albasser@syentys.com>
2018-10-24 09:37:50 +0200

    Added apitech_dependencies

--- Addons/custom/apitech_dependencies/__manifest__.py
+++ Addons/custom/apitech_dependencies/__manifest__.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Product Categories Default Code",
+    'name': "Apitech - Dependencies",
 
     'summary': """
-        Adds a default code to product categories""",
+        Install modules""",
 
     'description': """
     """,
@@ -14,11 +14,11 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Inventory',
+    'category': 'Technical Settings',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['product'],
+    'depends': ['crm', 'account', 'sale_managment', 'stock', 'mrp', 'purchase', 'web_studio', 'mrp_plm'],
     
-    'data': ['views/product_views.xml'],
+    'data': [],
 }
