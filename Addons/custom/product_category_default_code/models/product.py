e1afd524c654119564be17fde655481d05193cc1
Sylvain Albasser <sylvain.albasser@syentys.com>
2018-10-24 09:08:37 +0200

    Fix indent

--- Addons/custom/product_category_default_code/models/product.py
+++ Addons/custom/product_category_default_code/models/product.py
@@ -7,9 +7,9 @@ class ProductCategory(models.Model):
     _inherit = "product.category"
     
     default_code = fields.Char(u'Référence interne')
-	complete_default_code = fields.Char(
-		u'Référence interne complète', compute='_compute_complete_default_code',
-		store=True)
+    complete_default_code = fields.Char(
+        u'Référence interne complète', compute='_compute_complete_default_code',
+        store=True)
 
     @api.depends('default_code', 'parent_id.complete_default_code')
     def _compute_complete_default_code(self):
@@ -17,5 +17,5 @@ class ProductCategory(models.Model):
             if category.parent_id:
                 category.complete_default_code = '%s%s' % (category.parent_id.complete_default_code or '', category.default_code or '')
             else:
-				category.complete_default_code = category.default_code or ''
+                category.complete_default_code = category.default_code or ''
 
