9c1f2125365a11aa9f3bf3369bdd7cdcd8048070
Sylvain Albasser <sylvain.albasser@syentys.com>
2018-10-31 14:39:14 +0100

    [FIX] partner_prospect: fix xpath in form view

--- Addons/custom/partner_prospect/models/partner.py
+++ Addons/custom/partner_prospect/models/partner.py
@@ -6,4 +6,4 @@ from odoo import fields, models, api
 class ResPartner(models.Model):
     _inherit = "res.partner"
     
-    prospect = fields.Boolean('Est un prospect', default=False)
+    prospect = fields.Boolean('Est un Prospect', default=False)
