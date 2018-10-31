4ff054353628d7f34d50e755fc3fd5ddd104739d
Sylvain Albasser <sylvain.albasser@syentys.com>
2018-10-31 09:08:55 +0100

    [FIX] partner_prospect: dependency and search view

--- Addons/custom/partner_prospect/__manifest__.py
+++ Addons/custom/partner_prospect/__manifest__.py
@@ -15,10 +15,10 @@
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Sales',
-    'version': '0.1',
+    'version': '0.2',
 
     # any module necessary for this one to work correctly
-    'depends': ['sale_management'],
+    'depends': ['base'],
     
     'data': ['views/partner_views.xml'],
 }
